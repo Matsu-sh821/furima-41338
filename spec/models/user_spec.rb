0d42a5b6fe8297d5f949d0ec473374befd7c6cb9
Matsu-sh821 <m.shouta128@gmail.com>
2024-12-26 18:27:19 +0900

    ユーザー管理機能3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,27 +10,6 @@ RSpec.describe User, type: :model do
       it '全ての項目が存在すれば登録できる' do
         expect(@user).to be_valid
       end
-      it 'パスワード(password)が6文字以上半角英数字混合であれば登録できる' do
-        @user.password = '111aaa'
-        @user.password_confirmation = '111aaa'
-        expect(@user).to be_valid
-      end
-      it '名字(last_name)が全角（漢字・ひらがな・カタカナ）であれば登録できる' do
-        @user.last_name = '山田'
-        expect(@user).to be_valid
-      end
-      it '名前(first_name)が全角（漢字・ひらがな・カタカナ）であれば登録できる' do
-        @user.first_name = '太郎'
-        expect(@user).to be_valid
-      end
-      it '名字のふりがな(last_name_kana)が全角（カタカナ）であれば登録できる' do
-        @user.last_name_kana = 'ヤマダ'
-        expect(@user).to be_valid
-      end
-      it '名前のふりがな(first_name_kana)が全角（カタカナ）であれば登録できる' do
-        @user.first_name_kana = 'タロウ'
-        expect(@user).to be_valid
-      end
     end
 
     context '新規登録できない場合' do
