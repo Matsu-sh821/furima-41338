cfd06f3f2cad2a6ab4ab5e108c0282905252c5b8
Matsu-sh821 <m.shouta128@gmail.com>
2025-01-08 02:23:17 +0900

    商品購入機能1

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,47 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
+
+  def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @order_form = OrderForm.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(purchase_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :block_number, :apartment_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def non_purchased_item
+    @item = Item.find(params[:item_id])
+    return unless current_user.id == @item.user_id || @item.purchase.present?
+
+    redirect_to root_path
+  end
+end
